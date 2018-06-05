0e925588137da03f1564a5c36c925d9082cf4a5f
daniellabianca <db.colangelo0731@gmail.com>
2018-06-05 11:09:01 -0400

    fixed navbar and started vars for trivia questions

---
+++ script.js
@@ -0,0 +1,323 @@
+//easy questions array
+const easy = [
+    {
+        question: "What country owns every panda in the world?",
+        answers: {
+            a: "China",
+            b: "Japan",
+            c: "Korea",
+            d: "Australia"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "Lucy in the sky with ___:",
+        answers: {
+            a: "Sunshine",
+            b: "Diamonds",
+            c: "Stars",
+            d: "Rockets"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What was tested at \"Bikini Atoll\" in the 1940\'s and the 1950\'s?",
+        answers: {
+            a: "Nuclear bomb",
+            b: "Atomic bomb ",
+            c: "Stealth aircraft",
+            d: "Mustard gas"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which atmospheric gas is the most common?",
+        answers: {
+            a: "Hydrogen",
+            b: "Oxygen",
+            c: "Argon",
+            d: "Nitrogen"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Excuse me while I ___:",
+        answers: {
+            a: "Eat this pie",
+            b: "Kiss this guy",
+            c: "Kiss the sky",
+            d: "Go mile high"
+        },
+        correctAnswer: "c"
+    },
+  
+    {
+        question: "Which star of the film \"Rebel Without a Cause\" died at the age of 24?        ",
+        answers: {
+            a: "James Dean",
+            b: "Natalie Wood",
+            c: "Gregory Peck",
+            d: "Clark Gable"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "The Olympic flag\'s colors are always red, black, blue, green, and yellow rings on a field of white because __:",
+        answers: {
+            a: "At least one of those colors appears on the flag of every nation on the planet.",
+            b: "They are primary colors.",
+            c: "They have important symbolism.",
+            d: "Each color is owned by the United States."
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Wooly Mammoths were still alive when:",
+        answers: {
+            a: "The Mayan calendar began.",
+            b: "Barley and wheat were first cultivated.",
+            c: "The Great Pyramid of Giza was built.",
+            d: "The Bronze Age began."
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who wrote \"Where the Sidewalk Ends?\"",
+        answers: {
+            a: "Allen Ginsberg",
+            b: "Charles Bukowski",
+            c: "Dr Seuss",
+            d: "Shel Silverstein"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "\"Psycho\" was the first American film to show __",
+        answers: {
+            a: "Gory murder scenes",
+            b: "A couple in bed",
+            c: "A flushing toilet",
+            d: "Matricide"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What country owns every panda in the world?",
+        answers: {
+            a: "China",
+            b: "Japan",
+            c: "Korea",
+            d: "Australia"
+        },
+        correctAnswer: "a"
+    },
+];  
+const tough = [
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+},    
+
+];
+
+
+const impossible = [
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+    },
+    {
+        question: "What country owns every panda in the world?",
+    answers: {
+        a: "China",
+        b: "Japan",
+        c: "Korea",
+        d: "Australia"
+    },
+    correctAnswer: "a"
+},    
+];
